c8ef950806c7a9d7ab6904cb25079608d76b88bd
Ivan Corlli <corlliivan@gmail.com>
2021-11-02 10:21:52 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import NavBar from './components/NavBar/NavBar.js';
+// import NavBar from './components/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBarTranca from './components/NavBar/NavBarTranca'
+// import NavBarTranca from './components/NavBar/NavBarTranca';
+import NavBar from './components/navbar/NavBar';
+
 
 function App() {
   return (
     <>
-      <NavBarTranca/>
+      {/* <NavBarTranca/> */}
+      <NavBar/>
     </>
   );
 }
